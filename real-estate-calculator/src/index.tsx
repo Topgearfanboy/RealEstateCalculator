cb2f1e8b77425482ce1fa7b251851bfe872c3e2b
Mason Barden <masonleebarden@gmail.com>
2024-04-10 12:44:24 -0400

    cleanup

--- real-estate-calculator/src/index.tsx
+++ real-estate-calculator/src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import "./styles/tailwind.css";
-import * as serviceWorker from "./serviceWorker";
 import React from "react";
 import { AppContainer } from "./AppContainer";
 
@@ -9,8 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <AppContainer />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
